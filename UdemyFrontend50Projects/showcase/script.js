90965df4e0b195661072650a5c37e976a0020317
Garrett Becker <53883172+gdbecker@users.noreply.github.com>
2026-04-27 15:09:08 -0400

    Refactor addProjects to use async/await
    Refactored addProjects function to use async/await for fetching data from JSON file and added error handling.

--- UdemyFrontend50Projects/showcase/script.js
+++ UdemyFrontend50Projects/showcase/script.js
@@ -1,23 +1,29 @@
-import data from './data.json' assert { type: 'json' };
-
 const projects_container = document.querySelector('.projects');
 
-function addProjects() {
-	data.forEach(project => {
-		const p = document.createElement('div');
-		p.classList.add('card');
-		p.innerHTML = `
-			<div class="details">
-				<a href="${project.url}" target="_blank">${project.title}</a>
-				<p>${project.description}</p>
-			</div>
-			<div class="img-container">
-				<img src="${project.src}" alt="${project.alt}">
-			</div>
-		`;
-	
-		projects_container.appendChild(p);
-	});
+async function addProjects() {
+		try {
+				const response = await fetch('./data.json');
+				if (!response.ok) throw new Error('Could not fetch data');
+
+				const data = await response.json();
+
+				data.forEach(project => {
+						const p = document.createElement('div');
+						p.classList.add('card');
+						p.innerHTML = `
+								<div class="details">
+										<a href="${project.url}" target="_blank">${project.title}</a>
+										<p>${project.description}</p>
+								</div>
+								<div class="img-container">
+										<img src="${project.src}" alt="${project.alt}">
+								</div>
+						`;
+						projects_container.appendChild(p);
+				});
+		} catch (error) {
+				console.error('Error loading projects:', error);
+		}
 }
 
 addProjects();
