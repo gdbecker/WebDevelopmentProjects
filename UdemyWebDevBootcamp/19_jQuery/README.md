b41f24a8e8b393d2a47a85888e5fea891f0fe3a8
Garrett Becker <garrettdbecker@gmail.com>
2026-05-01 14:25:08 -0400

    Updates [skip netlify]

--- UdemyWebDevBootcamp/19_jQuery/README.md
+++ UdemyWebDevBootcamp/19_jQuery/README.md
@@ -1,6 +1,6 @@
 ## 19 - jQuery
 
-### [Demo: jQuery Practice](https://replit.com/@gdbecker/jQueryPractice)
+### [Demo: jQuery Practice](https://wdb-jquery-practice-gdbecker.netlify.app)
 
 Learning jQuery concepts through a small webpage - clicking on any of the buttons will collapse/show the text. Mousing over "goodbye" will change the color from black to orange.
 
--- UdemyWebDevBootcamp/23_NodeJS/QRCodeGenerator/index.js
+++ UdemyWebDevBootcamp/23_NodeJS/QRCodeGenerator/index.js
@@ -4,35 +4,69 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 
+// Local development version
+
+// import inquirer from "inquirer";
+// import qr from "qr-image";
+// import fs from "fs";
+
+// // Get user input
+// inquirer
+//   .prompt([
+//     {
+//       message: "Type in your URL:",
+//       name: "URL",
+//     },
+//   ])
+//   .then((answers) => {
+//     const url = answers.URL;
+
+//     // Convert user's url into QR code
+//     let qr_svg = qr.image(url);
+//     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+//     // Text file to save user input
+//     fs.writeFile("URL.txt", url, (err) => {
+//       if (err) throw err;
+//       console.log("The file has been saved!");
+//     });
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
+
+// Deployment version
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
 
-// Get user input
-inquirer
-  .prompt([
-    {
-      message: "Type in your URL:",
-      name: "URL",
-    },
-  ])
-  .then((answers) => {
-    const url = answers.URL;
+// Helper function to generate the files
+const generateFiles = (url) => {
+  let qr_svg = qr.image(url);
+  qr_svg.pipe(fs.createWriteStream("qr_img.png"));
 
-    // Convert user's url into QR code
-    let qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
-
-    // Text file to save user input
-    fs.writeFile("URL.txt", url, (err) => {
-      if (err) throw err;
-      console.log("The file has been saved!");
-    });
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+  fs.writeFile("URL.txt", url, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
   });
+};
+
+// Check if we are in a Build Environment (like Netlify)
+if (process.env.NODE_ENV === 'production' || process.env.CI) {
+  // Use a default URL for the portfolio build
+  generateFiles("https://github.com/gdbecker");
+} else {
+  // Run normal interactive mode for your local machine
+  inquirer
+    .prompt([{ message: "Type in your URL:", name: "URL" }])
+    .then((answers) => {
+      generateFiles(answers.URL);
+    })
+    .catch((error) => {
+      console.error("Inquirer error:", error);
+    });
+}
